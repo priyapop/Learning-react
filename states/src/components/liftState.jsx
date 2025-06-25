1eea2cbd0e432526b5597e8711b8d948a728f5e7
priyapop <dangolpriya21@gmail.com>
2025-06-25 16:44:03 +0545

    lifting state

---
+++ states/src/components/liftState.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const LiftingState = () => {
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <>
+      <InputComponent inputValue={inputValue} setInputValue={setInputValue} />
+      <DisplayComponent inputValue={inputValue} />
+    </>
+  );
+};
+const InputComponent = ({ inputValue, setInputValue }) => {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="name"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>
+    </>
+  );
+};
+
+const DisplayComponent = ({ inputValue }) => {
+  return <p>Your name is {inputValue}</p>;
+};
--- states/src/main.jsx
+++ states/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
-import { DeriveState } from './components/derivedstate.jsx';
+import { LiftingState } from './components/liftState.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DeriveState />
-  </StrictMode>,
+    <LiftingState />
+  </StrictMode>
 )
